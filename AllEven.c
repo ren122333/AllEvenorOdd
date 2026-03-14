2452c55f67c2428eacb592385338446d5603a783
ren122333 <danyabogdanovski@gmail.com>
2026-03-14 19:42:12 +0200

    MoreChaos

--- AllEven.c
+++ AllEven.c
@@ -5,10 +5,14 @@ int main() {
     printf("Please enter the numbers");
     scanf("%d, %d, %d", &a, &b, &d);
     if (a%2 == b%2 && b%2 == d%2){
-        printf("True");
+        if (1){
+            printf("True");
+        }
         return(1);
+    }
+    if (2 == 3){
+        printf("I owe you money.");
     }
     printf("False");
     return 0;
-    printf("I owe you money.");
 }
