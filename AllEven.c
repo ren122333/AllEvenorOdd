05746f7ad8589f48b37f5524d51614fdb9fbbee6
ren122333 <danyabogdanovski@gmail.com>
2026-03-14 19:09:18 +0200

    SecondTry

---
+++ AllEven.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ 
+int main() {
+    int a, b, c;
+    printf("Please enter the numbers");
+    scanf("%d, %d, %d", &a, &b, &c);
+    if (a%2 == b%2 && b%2 == c%2){
+        printf("True");
+        return(1);
+    }
+    printf("False");
+    return 0;
+}
